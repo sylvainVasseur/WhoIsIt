50315a3df0fd566449a18aea32dcfbceb8ff7d0a
Sylvain.Vasseur <sylvainvasseur2@gmail.com>
2022-06-09 16:51:25 +0200

    model liste question reponse

--- FrontWhoIsIt/src/app/models/liste-questions-reponses.model.ts
+++ FrontWhoIsIt/src/app/models/liste-questions-reponses.model.ts
@@ -1,2 +1,17 @@
-export interface ListeQuestionsReponses {
+export class ListeQuestionsReponses {
+
+    id: string;
+    Question: string;
+    Reponse: boolean;
+    Asked: boolean;
+
+    constructor(){
+        this.id ="";
+        this.Question = "";
+        this.Reponse = false;
+        this.Asked = false;
+
+    }
+
+
 }
