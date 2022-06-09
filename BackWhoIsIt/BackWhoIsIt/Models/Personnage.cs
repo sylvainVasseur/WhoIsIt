af74816fab48bc8abecdc42a85fd2968ba737f2e
Lionel Naye <ngonkouralionel@gmail.com>
2022-06-09 14:15:33 +0200

    init back models

---
+++ BackWhoIsIt/BackWhoIsIt/Models/Personnage.cs
@@ -0,0 +1,16 @@
+﻿namespace BackWhoIsIt.Models
+{
+    public class Personnage
+    {
+        public string Id { get; set; }
+        public string Genre { get; set; }
+        public string Espece { get; set; }
+        public string CouleurYeux { get; set; }
+        public string CouleurCheveux { get; set; }
+        public bool hasLunette { get; set; }
+        public bool hasChapeau { get; set; }
+        public bool isWizard { get; set; }
+        public string urlPhoto { get; set; }
+
+    }
+}
