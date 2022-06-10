b773657c09a6af7004c5053dc72e81c526835913
Stevie <stevie.leveque@cgi.com>
2022-06-10 13:55:45 +0200

    ajout questions

--- FrontWhoIsIt/src/app/components/game/game.component.ts
+++ FrontWhoIsIt/src/app/components/game/game.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ListeQuestionsReponses } from 'src/app/models/liste-questions-reponses.model';
 import { Personnages } from 'src/app/models/personnages.model';
+import { ListeQuestionsReponsesService } from 'src/app/services/liste-questions-reponses.service';
 import { PersonnageService } from 'src/app/services/personnage.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class GameComponent implements OnInit {
   nbQuestion : number = 0;
   NumeroPersonnageAI!: number;
 
-  constructor(private http: HttpClient, private router: Router, private service: PersonnageService,) { }
+  constructor(private http: HttpClient, private router: Router, private service: PersonnageService, private serviceListeQuestions: ListeQuestionsReponsesService) { }
 
   ngOnInit(): void {
 
@@ -26,6 +27,10 @@ export class GameComponent implements OnInit {
       console.log(this.listPersonnage[0]);
     });
     console.log("numero personnage : " + this.NumeroPersonnageAI);
+
+    this.serviceListeQuestions.getListeQuestionsReponses().subscribe(data =>{
+      this.listQuestion = data;
+    })
   }
 
 }
